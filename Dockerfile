974b0d4a14fb70ecc5d8cd04a61e9b10a4f64915
Aylar12 <ailar.jafarian@gmail.com>
2025-08-11 18:49:59 +0330

    change-2

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ WORKDIR /app
 COPY package*.json ./
 RUN npm install figlet
 COPY . .
+EXPOSE 3000
 CMD ["node", "nodex.js"]
--- nodex.js
+++ nodex.js
@@ -1,10 +1,18 @@
+const http = require("http");
 const figlet = require("figlet");
 
-figlet("hello Arii", function(err, data) {
-    if (err) {
-        console.log("error figlet");
-        console.dir(err);
-        return;
-    }
-    console.log(data);
+const PORT = 3000;
+
+http.createServer((req, res) => {
+    figlet("hello", function(err, data) {
+        if (err) {
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Error generating figlet");
+            return;
+        }
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end(data);
+    });
+}).listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
